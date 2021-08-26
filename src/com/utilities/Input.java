77c5307dddd8908725b384edc43ef326582587aa
johngillard15 <johngillard15@gmail.com>
2021-08-26 19:03:42 -0400

    added output for invalid inputs

--- src/com/utilities/Input.java
+++ src/com/utilities/Input.java
@@ -16,7 +16,7 @@ public class Input {
     public static int getInt(final int MIN){
         return getInt(MIN, Integer.MAX_VALUE);
     }
-    public static int getInt(final double MIN, final double MAX){
+    public static int getInt(final int MIN, final int MAX){
         String input;
 
         boolean validChoice;
@@ -26,6 +26,13 @@ public class Input {
 
             validChoice =
                     InputValidator.validInt(input) && (Integer.parseInt(input) >= MIN && Integer.parseInt(input) <= MAX);
+
+            if(!InputValidator.validInt(input))
+                System.out.println("That is not a valid number value. Please try again");
+            else if(MIN != Integer.MIN_VALUE && !(Integer.parseInt(input) >= MIN))
+                System.out.println("Value must be greater than or equal to " + MIN);
+            else if(MAX != Integer.MAX_VALUE && !(Integer.parseInt(input) <= MAX))
+                System.out.println("Value must be less than or equal to " + MAX);
         }while(!validChoice);
 
         return Integer.parseInt(input);
@@ -47,6 +54,13 @@ public class Input {
 
             validChoice =
                     InputValidator.validDouble(input) && (Double.parseDouble(input) >= MIN && Double.parseDouble(input) <= MAX);
+
+            if(!InputValidator.validDouble(input))
+                System.out.println("That is not a valid number value. Please try again");
+            else if(MIN != Double.MIN_VALUE && !(Double.parseDouble(input) >= MIN))
+                System.out.println("Value must be greater than or equal to " + MIN);
+            else if(MAX != Double.MAX_VALUE && !(Double.parseDouble(input) <= MAX))
+                System.out.println("Value must be less than or equal to " + MAX);
         }while(!validChoice);
 
         return Double.parseDouble(input);
