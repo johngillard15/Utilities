0191bc819f715eb9bde075ca661fa891e25f13a1
johngillard15 <johngillard15@gmail.com>
2021-09-02 18:58:17 -0400

    added Math class and tests
    + added tests for UI and Math
    + added check in Math for insufficient arguments

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,10 +1,16 @@
 package com.company;
 
+import com.utilities.CLI;
+import com.utilities.Math;
 import com.utilities.UI;
 
 public class Test {
 
     public static void main(String[] args) {
+        System.out.println(Math.max(1));
+
+        CLI.pause();
+
 	    // write your code here
         String[] arr = new String[69];
         for(int i = 0; i < arr.length; i++)
---
+++ src/com/utilities/Math.java
@@ -0,0 +1,32 @@
+package com.utilities;
+
+public class Math { // TODO: are these more efficient than arr.stream().max/min ?
+
+    public static int max(int... nums) throws IllegalArgumentException {
+        if(nums.length <= 1) throw new IllegalArgumentException("Math.max requires at least two arguments." +
+                " Argument length: " + nums.length);
+
+        int max = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] > max)
+                max = nums[i];
+        }
+
+        return max;
+    }
+
+    public static int min(int... nums) throws IllegalArgumentException {
+        if(nums.length <= 1) throw new IllegalArgumentException("Math.min requires at least two arguments." +
+                " Argument length: " + nums.length);
+
+        int min = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] > min)
+                min = nums[i];
+        }
+
+        return min;
+    }
+}
