8b9e38f61f99f3823f0a19c7f65d9384c901fddd
johngillard15 <johngillard15@gmail.com>
2021-09-19 16:14:10 -0400

    added test for roman numeral list type

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -16,9 +16,9 @@ public class Test {
     public static void testUI(){
         String[] arr = new String[69];
         for(int i = 0; i < arr.length; i++)
-            arr[i] = Integer.toString(i + 1);
+            arr[i] = "Element " + (i + 1);
 
-        UI.listerator(2, 1, arr);
+        UI.listerator(3, 1, arr);
     }
 
     public static void testConvert(){
@@ -41,6 +41,6 @@ public class Test {
 
     public static void main(String[] args) {
 	    // write your code here
-        testConvert();
+        testUI();
     }
 }
