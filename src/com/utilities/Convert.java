d5b30ed1b6710a4ead8fe02b8d68dcd37ef43ada
johngillard15 <johngillard15@gmail.com>
2021-09-19 16:13:18 -0400

    added roman numeral converter

--- src/com/utilities/Convert.java
+++ src/com/utilities/Convert.java
@@ -1,11 +1,39 @@
 package com.utilities;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Convert {
+    private static final TreeMap<Integer, String> numeralValues = new TreeMap<>();
+
+    private static void romanNumeralInit(){
+        numeralValues.put(1, "I");
+        numeralValues.put(4, "IV");
+        numeralValues.put(5, "V");
+        numeralValues.put(9, "IX");
+        numeralValues.put(10, "X");
+        numeralValues.put(40, "XL");
+        numeralValues.put(50, "L");
+        numeralValues.put(90, "XC");
+        numeralValues.put(100, "C");
+        numeralValues.put(400, "CD");
+        numeralValues.put(500, "D");
+        numeralValues.put(900, "CM");
+        numeralValues.put(1000, "M");
+    }
+
+    public static String toRomanNumeral(int num){
+        romanNumeralInit();
+        // TODO: handle each place separately then concatenate
+        return toRoman(num);
+    }
+
+    private static String toRoman(int num){
+        int highKey = numeralValues.floorKey(num);
+        if(num == highKey)
+            return numeralValues.get(num);
+        return numeralValues.get(highKey) + toRoman(num - highKey);
+    }
 
     public static int[] toArray(int... ints){
         return ints;
